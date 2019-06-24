0c5df88d145ebd42ed7b562bbb1c81a28581a483
PTaras <potsikaylot@gmail.com>
2019-06-24 18:18:00 +0300

    lessons 9

--- source/components/Postman/index.js
+++ source/components/Postman/index.js
@@ -1,17 +1,41 @@
 // Core
-import React from 'react';
+import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
+import { fromTo } from 'gsap';
 
-// Imstruments
-import Styles from './styles.m.css';
+//Components 
 import { withProfile } from 'components/HOC/withProfile';
 
-const Postman = (props) => {
-    return (
-        <section className = { Styles.postman }>
-            <img src = { props.avatar } />
-            <span>Welcome online, {props.currentUserFirstName}</span>
-        </section>
-    );
-};
+// Instruments
+import Styles from './styles.m.css';
 
-export default withProfile(Postman);
+@withProfile
+export default class Postman extends Component {
+
+    _animatePostmanEnter = (Postman) => {
+        fromTo(Postman, 2, { opacity: 0, x: 500 }, { opacity: 1, x: 0 })
+    };
+    
+    _animatePostmanExit = (Postman) => {
+        fromTo(Postman, 2, { opacity: 1, x: 0 }, { opacity: 0, x: 500 })
+    };
+    
+    render () {
+
+        const { avatar, currentUserFirstName } = this.props;
+
+        return (
+            <Transition
+                appear 
+                in 
+                timeout = { 4000 }
+                onEnter = { this._animatePostmanEnter }
+                onEntered = { this._animatePostmanExit }>   
+                <section className = { Styles.postman }>
+                    <img src = { avatar } />
+                    <span>Welcome online, {currentUserFirstName}</span>
+                </section>
+            </Transition>
+        );
+    }
+}
